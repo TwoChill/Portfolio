6345b2b6a433770ca84a3d812d56f44391a6c5a0
Michael <michaeldefrance@gmail.com>
2019-06-25 17:32:11 +0200

    Made some comments

--- Room_Exit_Game.py
+++ Room_Exit_Game.py
@@ -3,8 +3,8 @@
 
 
 ########################################################################################################################################
-                                                        PART OF AN EXERCISE
-                                                        ###################
+#                                                        PART OF AN EXERCISE
+#                                                        ###################
 locations = {0: "You are sitting in front of a computer learing Python",
 			 1: "You are standing at the end of a road, a small brick building",
 			 2: "You are at the top of a hill",
@@ -18,9 +18,9 @@ exit = [{"Quit": 0},
 		{"East": 1, "Quit": 0},
 		{"West": 2, "North": 1, "Quit": 0},
 		{"West": 2, "South": 1, "Quit": 0}]
- 
-                                                        ###################
-                                                        PART OF AN EXERCISE
+
+#                                                        ###################
+#                                                        PART OF AN EXERCISE
 ########################################################################################################################################
 
 loc = 1
