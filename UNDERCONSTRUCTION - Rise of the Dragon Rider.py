e8ba0d0d28d8f9efee7a4b192755a98454d5eb43
Michael <michaeldefrance@gmail.com>
2019-06-30 15:53:36 +0200

    Outline continue

--- UNDERCONSTRUCTION - Rise of the Dragon Rider.py
+++ UNDERCONSTRUCTION - Rise of the Dragon Rider.py
@@ -35,8 +35,10 @@ def help():
 #        print('- Use "look" to inspect surrounding  ')
         print('- Use "dig" to investigate area  ')
         print('- Good luck and have fun!  ')
-        print('\n############################\n')
+        print('\n############################')
+
         help_answer = str(input('\nPress ENTER to continue\n:>'))
+
         while True:
             if help_answer == "":
                 start(usrName)
@@ -66,44 +68,62 @@ def player_info():
 
 def home(usrName, location):
     print(f'\n\nWelcome {usrName} you are {location}.')
-
     # Your home
     # Your mentor
     # Your room with Lore read    functie
 
-# def outside_home():
-#     # Serrounding like god of war
-#     # go to central
-#     # dig
-#
-# def mentor(location):
-#     # parameter tells were you are in game and what to print
-#     # go outside and inside get basic_quest to dig outside find map() for mentor.
-#     ##got_quest = True (and shows in menu)
-#
-# def dig(location, quest1, quest2, quest3):
-#     #  can dig anywhere to find something or certain lores
-#     #  droprate of items depends on were you are in stroy
-#     #  depending on location, items to find
-#     #  there's a time delay for this action
-#
-# def shoptrader(location):
-#     # first static at calmlands()
-#     # later based on player location moves around inc 3 or 4.
-#     # when shoptraders moves, has found mysterious stone, gives it to you for some gil
-#
-# def menu(got_quest, got_spellbook, lorefound)():
-#     # Inventory()
-#     # Map()
-#     # Quest()
-#     # Spellbook()
-#     # lore_books('all')
-#     # help
-#
-# def lore_books(book_nr):
-#     # book_nr == 1:
-#     #   print(''' Book 1 ''')
-#     #  etc
+def outside_home():
+    # Serrounding like god of war
+    # go to central
+    # dig
+
+def usrName_room():
+    # here are some books with lore
+    # rest (saves the game)
+
+def mentor(location, quest1, quest2, quest3,):
+    # parameter tells were you are in game and what to print
+    # go outside and inside get basic_quest to dig outside find map() for mentor.
+    ##got_quest = True (and shows in menu)
+    # asked about quest and gives a hint an what to do next?
+
+def save_game():
+    # ask to overwrite or create new save.
+
+def auto_save():
+    # auto saves afther certain points and quests
+
+def progression():
+    # keeps a record of the main story so far
+    # order can change depending on how the game is played.
+
+def dig(location, quest1, quest2, quest3):
+    #  can dig anywhere to find something or certain lores
+    #  droprate of items depends on were you are in stroy
+    #  depending on location, items to find
+    #  there's a time delay for this action
+    # some books are only found throudigging
+
+def shoptrader(location):
+    # first static at calmlands()
+    # later based on player location moves around inc 3 or 4.
+    # when shoptraders moves, has found mysterious stone, gives it to you for some gil
+
+def menu(got_quest, got_spellbook, lorefound)():
+    # Inventory()
+    # Map()
+    # Quest()
+    # Spellbook()
+    # lore_books('all')
+    # Progression
+    # Save game
+    # help
+
+def lore_books(location, book_nr):
+    # book_nr == 1:
+    #   print(''' Book 1 ''')
+    #  etc
+    # some books are only found through digging
 
 usrName = ''
 start(usrName)
