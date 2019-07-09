f0877fcfa6c28ba5c9640002212edf26010380c9
Michael <michaeldefrance@gmail.com>
2019-07-09 20:19:57 +0200

    More DRY, Copy to Clipboard

--- Message_Coverter.py
+++ Message_Coverter.py
@@ -1,55 +1,62 @@
+import pyperclip # $ pip install pyperclip
+
 def convertword(convertedMSG):
 
     convertedMSG = convertedMSG.split(",")
 
+    # These are the letters to concatenate with the SecretWord.
+    ISHvowelList = (("oei", "aa","ae","ee","ae","oo",
+     "uu", "au", "ei","eu", "ie", "ou", "oe", "ui", "ij","io"),
+     ("a", "e", "o", "u", "i", "y"))
+
     for words in convertedMSG:
-        for letters in ISHvowelList[1]:
+        for letters in ISHvowelList[0]:
             if letters in words:
                 replace = secretWord + letters.upper()
                 words = words.replace(letters, replace)
 
+
         for word in words:
             for letter in words:
-                if letter in ISHvowelList[0]:
+                if letter in ISHvowelList[1]:
                     replace = secretWord + letter.upper()
                     words = words.replace(letter, replace)
-
-    #words = words.replace(".",".\n")
-    # print(">>> This is end of conversion: ", words)
     return words
-
-
-
-convertedMSG = str(input("Please enter your message:\n\n"))
-
-ISHvowelList = (
-  ("a", "e", "o", "u", "i", "y"),
-  ("oei", "aa", "ee", "oo", "uu", "au", "ei", "eu", "ie", "ou", "oe", "ui", "ij","io")
-)
 
 secretWord = "ISH"
 
-convertedMSG = convertedMSG.replace(",","").lower().split(" ")
+try:
+    convertedMSG = str(input("\nPlease enter your message:\n\n"))
 
-new_converterdMSG = []
-for index in range(0,(len(convertedMSG))):
-    new_converterdMSG.append(convertword(convertedMSG[index]))
+    # Removes comma's. This causes errors otherwise in the program.
+    convertedMSG = convertedMSG.replace(",","").lower().split(" ")
+
+    # The manipulated words are appended to 'new_converterdMSG'.
+    new_converterdMSG = []
+    for index in range(0,(len(convertedMSG))):
+        new_converterdMSG.append(convertword(convertedMSG[index]))
+
+    print("\n\n\nYour message in " + secretWord + ": \n")
+
+    # Converts words in a list to a string.
+    convMSG = " ".join(new_converterdMSG).capitalize()
+
+    # Makes every sentence begin with a capital letter and end with a dot.
+    sentence = convMSG.split('.')
+    for i in sentence:
+        print (i.strip().capitalize() + ". ", end='')
 
 
-print("\n\nYour message in " + secretWord + ": \n")
-print(" ".join(new_converterdMSG).capitalize())
+    answer = str(input('\n\n\nCopy to clipboard? Y/N: ').upper())
+    if answer == 'Y':
+        pyperclip.copy(convMSG)
+        print('\nMessage has been copied.')
+        raise KeyboardInterrupt
+    else:
+        print('\nMessage has NOT been copied.')
+        raise KeyboardInterrupt
+    print('\n')
 
 
-# again =  "N"
-# while again == "N":
-# 	again = (input("Do you want to convert another message to ISH? Y/N: "))
-# 	again = again.capitalize()
-# 	if again == "N":
-# 		print("\nThank you, and goodbye!")
-# 		break
-# 	if again == "Y":
-# 		convert2ish(convertedMSG = (input("\n\nWich message do you want to convert again to ISH?:\n\n")))
-# 		again = "N"
-# 	else:
-# 		print("Please use only \"N\" or \"Y\":\n")
-# 		again = "N"
+except KeyboardInterrupt:
+    print("\nGoodbye!\n")
