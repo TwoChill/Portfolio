4e1086e1af603b0890e6e6c567a06bc5f57c9b06
Oren Eno <Oren@localhost.localdomain>
2019-08-18 19:58:27 +0000

    STill in Progress.. Trying to correctly add a progress bar

---
+++ wordList_Generator.py
@@ -0,0 +1,76 @@
+import itertools
+import time
+from tqdm.auto import tqdm
+
+print('\n++++++++++++++++++++++++ Wordlist Gen +++++++++++++++++++++++++\n')
+
+
+char = input("\t[+] Please Enter Here All Word For Combination :> ")
+print('\n')
+rng1 = int(input("\t[+] Please Enter Minimum Lenth Of Words :> "))
+print('\n')
+rng2 = int(input("\t[+] Please Enter Maximum Lenth Of Words :> "))
+print('\n')
+
+exclude = int(
+    input("\t[+] Please Enter Sequential Charachter Exclution :> "))
+exclude -= 1
+if exclude < 0:
+    exclude = len(char)
+
+time1 = time.asctime()
+start = time.time()
+print('\n')
+output_file = input("\t[+] Please Enter The Path Of Wordlist File :> ")
+print('\n')
+
+# A for-loop to calculate max number of line in output_file.
+# What needs to happen is INCLUDE 'exclude' in the math.
+p = []
+for lines in range(rng1, (rng2 + 1)):
+    ans = (len(char)**lines)
+    p.append(ans)
+tline = sum(p)
+print("\t[+] Numbers Of Total Lines :> ", tline, "\n\n")
+
+input('\r\r\t[+] Are You Ready? [Press Enter]')
+
+print('\n++++++++++++++++++++++++ Please Wait +++++++++++++++++++++++++\n')
+
+wordList = open(output_file, 'a')
+with tqdm(total=tline, position=0) as loop:
+
+    for i in range(len(char), (len(char) + 1)):
+        for xt in itertools.product(char, repeat=i):
+            loop.set_description('Processing.... ')
+            cnts = [sum(1 for i in grp[1]) for grp in itertools.groupby(xt)]
+            if max(cnts) > exclude:
+                loop.update(1)
+                continue
+            wordList.write(''.join(xt) + '\n')
+            loop.update(1)
+    wordList.close()
+    loop.close()
+
+print("\nDone Sucessfully\n")
+
+print('\n\n+++++++++++++++++++++++ Process Report +++++++++++++++++++++++\n')
+
+print('\t [+] Process Started                      :   ', time1)
+end = time.time()
+print('\t [+] Process Completed                    :   ', time.asctime())
+totaltime = end - start
+print('\t [+] Total Time Consumed                  :   ', totaltime, 'seconds')
+rate = tline / totaltime
+print('\t [+] Rate Of Words Generating Per Seconds :   ', rate)
+
+print('\n++++++++++++++++++++++++ Please Wait +++++++++++++++++++++++++\n')
+
+print('''
+\t***************************************************
+\t*       Successfully created thanks for using     *
+\t***************************************************
+
+''')
+print('\n')
+input("[*] Press Enter For Exit")
