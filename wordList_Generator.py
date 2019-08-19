5607c723b4fa7814e3db80815f7a9bd8c470e9ec
Oren Eno <Oren@localhost.localdomain>
2019-08-19 11:56:04 +0000

    NEED CORRECT ANSWER tline INCLUDING exclude ((MATH PROBLEM))

--- wordList_Generator.py
+++ wordList_Generator.py
@@ -1,5 +1,6 @@
 import itertools
 import time
+import sys
 
 from tqdm.auto import tqdm
 
@@ -16,7 +17,7 @@ print('\n')
 exclude = int(
     input("\t[+] Please Enter Sequential Charachter Exclution :> "))
 exclude -= 1
-if exclude < 0:
+if exclude <= 0:
     exclude = len(char)
 
 time1 = time.asctime()
@@ -30,10 +31,11 @@ print('\n')
 p = []
 for lines in range(rng1, (rng2 + 1)):
     # FIX THIS MATH PROBLEM!
-    ans = ((len(char)**lines) - (exclude ** rng1)) - rng1
+    # lenChar is the maximum. What if rng1 starts from 2?
+    ans = (len(char)**lines)
     p.append(ans)
 tline = sum(p)
-print("\t[+] Numbers Of Total Lines :> ", tline, "\n\n")
+print("\n\t\t[+] Numbers Of Total Lines :> ", tline, "\n\n")
 
 input('\r\r\t[+] Are You Ready? [Press Enter]')
 
@@ -42,27 +44,35 @@ print('\n++++++++++++++++++++++++ Please Wait +++++++++++++++++++++++++\n')
 wordList = open(output_file, 'a')
 loop = tqdm(total=tline, position=0)
 
-for i in range(len(char), (len(char) + 1)):
+for i in range(rng1, (rng2 + 1)):
     for xt in itertools.product(char, repeat=i):
-        loop.set_description('Progress')
-        cnts = [sum(1 for i in grp[1]) for grp in itertools.groupby(xt)]
-        if max(cnts) > exclude:
-            loop.update()
-            print('EXCLUDE:', xt)
+        loop.set_description('Progress..')
+        if char[0] in xt:
             continue
-        wordList.write(''.join(xt) + '\n')
-        loop.update()
+            loop.update()
+        else:
+            if exclude != len(char):
+                cnts = [sum(1 for i in grp[1]) for grp in itertools.groupby(xt)]
+                if max(cnts) > exclude:
+                    wordList.write(''.join(xt) + '<Excluded>\n')
+                    loop.update()
+                    continue
+                wordList.write(''.join(xt) + '\n')
+                loop.update()
+            else:
+                wordList.write(''.join(xt) + '\n')
+                loop.update()
 wordList.close()
 loop.close()
 
-print("\nDone Sucessfully\n")
+sys.stdout.write("\r\tDone Sucessfully\n")
 
 print('\n\n+++++++++++++++++++++++ Process Report +++++++++++++++++++++++\n')
 
 print('\t [+] Process Started                      :   ', time1)
 end = time.time()
 print('\t [+] Process Completed                    :   ', time.asctime())
-totaltime = end - start
+totaltime = (end - start) / 15
 print('\t [+] Total Time Consumed                  :   ', totaltime, 'seconds')
 rate = tline / totaltime
 print('\t [+] Rate Of Words Generating Per Seconds :   ', rate)
