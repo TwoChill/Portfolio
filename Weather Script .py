219b5de5f7b9fd9a12d4f6c61ac40f5b34ea13a3
MdeFrance <michaeldefrance@gmail.com>
2019-11-21 15:43:24 +0100

    README
    # Weather Balloon
    Run this little script to:
    1. Convert Celcius to Fahrenheit or Fahrenheit to Celcius.
    2. Show the current weather information for your selected location.
    
    ## Getting Started
    
    ### Installing
    
    Clone the WeatherBalloon repository in terminal (or use your text editor):
    ```
    git clone https://github.com/TwoChill/WeatherBalloon.git
    ```
    
    Run the script with Python
    
    
    ## Authors
    
    * **Michael Leslie de France** - *Initial work* - [TwoChill](https://github.com/TwoChill)

--- Weather Script .py
+++ Weather Script .py
@@ -1,18 +1,22 @@
 from pathlib import Path
+import sys
+import requests
 import platform
 import time
 import os
 
+txtSpeed = 0.05
 # This block gets the filename and uses it in the program.
 # It doesn't matter were the file is located or what the filename is or will be.
 filePath = str(Path(__file__).absolute())
 fileExtention = filePath[len(filePath) - 4:]
 directoryPath = str(os.path.dirname(os.path.abspath(__file__)))
 
-scriptName = filePath[(len(directoryPath) + 1):-len(fileExtention)] + '\n\n\n'
+scriptName = '\t\t' + \
+    filePath[(len(directoryPath) + 1):-len(fileExtention)] + '\n\n\n'
 option1Name = 'Current Weather'
-option2Name = 'Temprature Converter'
-menuName = 'Menu'
+option2Name = '\tTemprature Converter'
+menuName = '\tMenu'
 
 
 def sys_clear(name=None):
@@ -25,7 +29,7 @@ def sys_clear(name=None):
         os.system('cls')
     else:
         # !!! Try to make a code that sends a message to your twitter. Just because you can.
-        print("Sorry, Your OS is not known to me yet.")
+        Typing(txtSpeed, "Sorry, Your OS is not known to me yet.")
 
     # This function can take more the 1 text to be displayed after a system clear call.
     if name == None:
@@ -33,14 +37,10 @@ def sys_clear(name=None):
     else:
         name = ''.join(name).split(',')
         for i in name:
-            print(i)
+            Typing.text_decor(None, ['bold', 'underline'], i)
 
 
 class WeatherBaloon(object):
-    # localWeather arg might be for data that we'll get from the API outside of the class
-
-    def __init__(self, localWeather):
-        self.localWeather = localWeather
 
     @staticmethod
     def menu():
@@ -50,21 +50,15 @@ class WeatherBaloon(object):
 
         try:
             answer = int(input('''
-1. Current Weather
+1. Weather Baloon
 2. Temprature Converter (°C <---> °F )
-
 
 3. Quit
 
 :> '''))
-
             while True:
                 if answer == 1:
-                    # WeatherBaloon.get_localweahter()
-                    sys_clear([scriptName, option1Name])
-                    print("\nI am working on this. Comming soon")
-                    itime.sleep(5)
-                    WeatherBaloon.menu()
+                    WeatherBaloon.get_localweahter()
                     break
                 elif answer == 2:
                     WeatherBaloon.get_tConv(option2Name)
@@ -73,16 +67,156 @@ class WeatherBaloon(object):
                     break
                 else:
                     WeatherBaloon.menu()
-        except:
-            if ValueError:
-                print("\n\nThat's not a number\n")
-                time.sleep(3)
-                WeatherBaloon.menu()
+        except (ValueError, KeyboardInterrupt):
+            print("\nThat's not a number!")
+            time.sleep(3)
+            WeatherBaloon.menu()
 
-    @classmethod
-    def get_localweahter(cls):
-        ''' Get's local weather data from OpenWeather's API '''
-        pass
+    @staticmethod
+    def get_localweahter():
+        ''' Get's local weather data from a weather API '''
+
+        sys_clear([scriptName, option1Name])
+
+        try:
+            answer = input(
+                "\nFor which " + bcolors.UNDERLINE + "location" + bcolors.ENDC + " do you want to get the current weather information?\n\n:> ").capitalize()
+
+            api_accesskey = f'http://api.weatherstack.com/current?access_key=f4ec8c3283872a7de69e9ec1129bfebf&query={answer}'
+
+            json_data = requests.get(api_accesskey).json()
+            # print(json_data)
+
+            city = ''.join([json_data['location']['name']])
+            country = ''.join([json_data['location']['country']])
+            # country_city = ','.join(x for x in [json_data['request']['query']])
+
+            # Clears the screen, displays special text. DO NOT MOVE THIS LINE UP!
+            sys_clear([scriptName, option1Name, ' in ', city, ' - ', country])
+
+            temperature = ''.join(str(x)
+                                  for x in [json_data['current']['temperature']])
+
+            weather_description = ''.join(
+                [json_data['current']['weather_descriptions'][0]])
+
+            observation_time = [json_data['current']['observation_time']][0]
+
+            local_time = ''.join(
+                [json_data['location']['localtime']]).split()[1] + ' ' + observation_time.split()[1]
+
+            pressure = ''.join(str(x)
+                               for x in [json_data['current']['pressure']])
+            humidity = ''.join(str(x)
+                               for x in [json_data['current']['humidity']])
+            cloudcover = ''.join(str(x)
+                                 for x in [json_data['current']['cloudcover']])
+            feelslike = ''.join(str(x)
+                                for x in [json_data['current']['feelslike']])
+            precip = ''.join(str(x) for x in [json_data['current']['precip']])
+
+            visibility = ''.join(str(x)
+                                 for x in [json_data['current']['visibility']])
+            wind_speed = ''.join(str(x)
+                                 for x in [json_data['current']['wind_speed']])
+            wind_degree = ''.join(
+                str(x) for x in [json_data['current']['wind_degree']]) + '°'
+
+            wind_dir = ''.join([json_data['current']['wind_dir']])
+
+            if wind_dir == 'S':
+                wind_dir = 'South'
+            elif wind_dir == 'SSE':
+                wind_dir = 'South / South East'
+            elif wind_dir == 'SE':
+                wind_dir = 'South East'
+            elif wind_dir == 'ESE':
+                wind_dir = 'East / South East'
+            elif wind_dir == 'E':
+                wind_dir = 'East'
+            elif wind_dir == 'ENE':
+                wind_dir = 'East / North East'
+            elif wind_dir == 'NE':
+                wind_dir = 'North East'
+            elif wind_dir == 'NNE':
+                wind_dir = 'Norht / North East'
+            elif wind_dir == 'N':
+                wind_dir = 'North'
+            elif wind_dir == 'NNW':
+                wind_dir = 'North / North West'
+            elif wind_dir == 'NW':
+                wind_dir = 'Nort West'
+            elif wind_dir == 'WNW':
+                wind_dir = 'West / North West'
+            elif wind_dir == 'W':
+                wind_dir = 'West'
+            elif wind_dir == 'WSW':
+                wind_dir = 'West / South West'
+            elif wind_dir == 'SW':
+                wind_dir = 'South West'
+            elif wind_dir == 'SSW':
+                wind_dir = 'South / South West'
+
+            unit = ''.join(json_data['request']['unit'])
+
+            if unit == 'm':
+                unit = 'C'
+            elif unit == 'f':
+                unit = 'F'
+            elif unit == 's':
+                unit = 'S'
+
+            print(f'''
+Local Time:\t\t {local_time}
+Observation Time:\t {observation_time}
+
+Temprature:\t\t {temperature}°{unit} - {weather_description}
+Feels Like:\t\t {feelslike}°{unit}
+
+Wind Speed:\t\t {wind_speed} Kph
+Wind Degree:\t\t {wind_degree}
+Wind Direction:\t\t {wind_dir}
+
+Cloud Cover:\t\t {cloudcover}%
+Precipitation:\t\t {precip}%
+Air Pressure:\t\t {pressure} MB (millibar)
+Humidity:\t\t {humidity}%
+Visibility:\t\t {visibility} Km
+''')
+
+            input('\n\t\t' + bcolors.BOLD +
+                  bcolors.UNDERLINE + 'CONTINUE' + bcolors.ENDC)
+
+            sys_clear([scriptName, option1Name])
+
+            while True:
+                answer = input('\nSelect another location?: (Y/N)\n\n:> ')
+
+                if answer.lower() in ('y', 'yes', 'ok', 'oke', 'okee', 'sure', 'cool'):
+                    WeatherBaloon.get_localweahter()
+                    break
+                else:
+                    sys_clear([scriptName, option1Name])
+                    answer = input('\nBack to main menu?: (Y/N)\n\n:> ')
+
+                    if answer.lower() in ('y', 'yes', 'ok', 'oke', 'okee', 'sure', 'cool'):
+                        WeatherBaloon.menu()
+                        break
+                    else:
+                        sys_clear([scriptName, option1Name])
+                        WeatherBaloon.quit()
+                        break
+                    break
+        except KeyboardInterrupt:
+            WeatherBaloon.quit()
+        except (ValueError):
+            print('\nThat is not a number!')
+            time.sleep(3)
+            WeatherBaloon.get_localweahter()
+        except (KeyError):
+            print("\nI don't know were or what that is.")
+            time.sleep(3)
+            WeatherBaloon.get_localweahter()
 
     @staticmethod
     def get_tConv(option2Name):
@@ -131,20 +265,94 @@ class WeatherBaloon(object):
                 answer = input('\nContinue?: (Y/N)\n\n:> ')
                 if answer.lower() in ('y', 'yes', 'ok', 'oke', 'okee', 'sure', 'cool'):
                     WeatherBaloon.menu()
+                    break
                 else:
                     sys_clear([scriptName, option2Name])
-                    WeatherBaloon.quit()
-        except:
-            if ValueError:
-                print("\n\nThat's not a number\n")
-                time.sleep(3)
-                WeatherBaloon.menu()
+                    answer = input('\nBack to main menu?: (Y/N)\n\n:> ')
+                    if answer.lower() in ('y', 'yes', 'ok', 'oke', 'okee', 'sure', 'cool'):
+                        WeatherBaloon.menu()
+                        break
+                    else:
+                        sys_clear([scriptName, option2Name])
+                        WeatherBaloon.quit()
+        except (ValueError, KeyboardInterrupt):
+            WeatherBaloon.quit()
 
     @staticmethod
     def quit():
         sys_clear(scriptName)
-        print('\nGoodbye\n')
-        quit()
+        print('Goodbye\n')
+        quit(0)
+
+
+class bcolors(object):
+    ''' Text colors to use in the Terminal '''
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+class Typing(bcolors):
+    ''' Game txt is being typed on screen. CTRL to increase speed '''
+
+    def __init__(self, txtSpeed, text=None):
+        self.text = text
+
+        if text is None:
+            self.text == []
+        else:
+            self.text = text
+
+        self.txtSpeed = txtSpeed
+        self.bcolors = bcolors
+
+        text = ''.join(self.text)
+
+        for l in text:
+            sys.stdout.write(l)
+            sys.stdout.flush()
+            time.sleep(txtSpeed)
+
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+
+    @classmethod
+    def text_decor(cls, color, decor=None, text=None):
+        ''' This function decorates text with UNDERLINE and/or BOLD '''
+
+        if text == None:
+            text = []
+        elif color == 'red':
+            text = bcolors.FAIL + text + bcolors.ENDC
+        elif color == 'green':
+            text = bcolors.OKGREEN + text + bcolors.ENDC
+        elif color == 'blue':
+            text = bcolors.OKBLUE + text + bcolors.ENDC
+        elif color == 'orange':
+            text = bcolors.WARNING + text + bcolors.ENDC
+        else:
+            text = text + bcolors.ENDC
+
+        if decor == None:
+            decor == []
+        elif decor == ['bold', 'underline']:
+            for i in decor:
+                if i == 'bold':
+                    text = bcolors.BOLD + text
+                if i == 'underline':
+                    text = bcolors.UNDERLINE + text
+        else:
+            if decor == 'bold':
+                text = bcolors.BOLD + text
+        if decor == 'underline':
+            text = bcolors.UNDERLINE + text
+
+        print(text)
 
 
 WeatherBaloon.menu()
