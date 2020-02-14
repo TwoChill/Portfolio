380ba68b6fa3a07a41cd18857da06fef1bb16654
MdeFrance <mdefrance1988@gmail.com>
2020-02-14 01:57:26 +0100

    Add Class bcolors
    Wacht out for use whit an argv. If there's only one directory, the script will change permissions without asking for confirmation.

--- Quility_of_Life_Scripts/Change_Permissions.py
+++ Quility_of_Life_Scripts/Change_Permissions.py
@@ -1,3 +1,5 @@
+#!/bin/python3.7
+
 from pathlib import Path
 import time
 import platform
@@ -5,6 +7,7 @@ import os
 import sys
 import stat
 import itertools
+
 # The script functionality changes depending on the variable argument.
 from sys import argv
 if len(argv) > 1:
@@ -68,44 +71,59 @@ class MyClass(object):
             print(b, end="\r")
             time.sleep(.5)
 
-    def change_permissions(self, path, argv_pressent):
+    def change_permissions(self, path, path_basename, argv_pressent):
         if 'linux' in platform.platform().lower():
             os.system('ls -d  -al */')
-
-        # Get basename of 1 folder INSIDE os.getcwd()
-        basename = f'{os.path.basename(path)}'
 
         # ask for OCTALS if argv is NOT FOUND:
         if argv_pressent is False:
 
             choose_flag = f'''
-Which \'Flag\' should be assigned to \'{basename}\':
+Choose a \'''' + bcolors.BOLD + '''Flag''' + bcolors.ENDC + '''\' to be assigned to''' + bcolors.BOLD + ''' ALL''' + bcolors.ENDC + f''' files --> \'{os.path.basename(path)}\':''' + bcolors.FAIL + '''
 
     User:\tr/w/x\t-->\t4/2/1\tsum = 7
     Group:\tr/w/x\t-->\t4/2/1\tsum = 7
     Other:\tr/w/x\t-->\t4/2/1\tsum = 7
 
-(IF YOU DON\'T KNOW WHAT FLAGS ARE, PLEASE DISCONTINUE!)'''
+''' + bcolors.ENDC + bcolors.BOLD + '''
+(IF YOU DON\'T KNOW WHAT FLAGS ARE, PLEASE DISCONTINUE!)''' + bcolors.ENDC
 
             sys.stdout.write(choose_flag)
             sys.stdout.flush()
 
-            # Confirmation
             while True:
-                # Clear Screen with printstatement
+
+                # Clear Screen
                 sys_clear()
-                os.system("ls -d -al */")
+                if 'linux' in platform.platform().lower():
+
+                    # Show current flag permissions of directory
+                    os.system("ls -d -al */")
+
                 sys.stdout.write(choose_flag)
                 sys.stdout.flush()
 
-                usr_flag = int(input('\n\n\nEnter Flag :>\t'))
+                try:
+                    usr_flag = int(input('\n\n\nEnter Flag :>\t'))
+                except ValueError:
+                    print('\nNot a valid input!')
+                    time.sleep(3)
 
+                    # Clear Screen
+                    sys_clear()
+
+                    continue
+
+                # Checks if entered flag is a actual flag
                 if str(usr_flag) in flag_combinations():
                     pass
                 else:
-                    # Clear Screen with printstatement
+                    # Clear Screen
                     sys_clear()
+
+                    # Show current flag permissions of directory
                     os.system("ls -d -al */")
+
                     sys.stdout.write(choose_flag)
                     sys.stdout.flush()
 
@@ -115,35 +133,61 @@ Which \'Flag\' should be assigned to \'{basename}\':
                     time.sleep(3)
                     continue
 
-                # Clear Screen with printstatement
+                # Clear Screen
                 sys_clear()
+
+                # Show current flag permissions of directory
                 os.system("ls -d -al */")
+
                 sys.stdout.write(choose_flag)
                 sys.stdout.flush()
 
                 answer = input(
                     f'\n\n\nEntered Flag :>\t\'{usr_flag}\'\n\n\nAre You Sure? (Y/N) :> ').upper()
 
-                # AFTHER AND IS A MINI-FIX OR WORKAROUND. SHOULD ACCEPT ONLY CHMOD OCTAL AND NOT JUST ANY 3 DIGITS.
                 if answer in ('Y', 'YES'):
 
                     if 'linux' in platform.platform().lower():
+                        
+                        # Clear Screen
                         sys_clear()
+
+                        # Show current flag permissions of directory
                         os.system('ls -d  -al */')
 
                         self.loadingAnimation(loadingTxt, True)
+                        
+                        # Get correct flag
+                        usr_flag = flag_interpreter(usr_flag, flags)
 
-                        os.chmod(os.getcwd(), usr_flag)
+                        # Change file permissions in parent directory
+                        for file in os.listdir(path):
+                            os.chmod(f'{path}/{file}', usr_flag)
+                        
+                        # Change parent directory permissions
+                        os.chmod(f'{path}', usr_flag)
+
+                        # Clear Screen
                         sys_clear()
-                        os.system("ls -d  -al */")
 
+                        # Show current flag permissions of directory
+                        os.system("ls -d  -al */")
                         break
 
                     # Other platforms
                     else:
                         sys_clear()
                         self.loadingAnimation(loadingTxt, True)
-                        os.chmod(os.getcwd(), usr_flag)
+
+                        # Get correct flag
+                        usr_flag = flag_interpreter(usr_flag, flags)
+
+                        # Change file permissions in parent directory
+                        for file in os.listdir(path):
+                            os.chmod(f'{path}/{file}', usr_flag)
+                        
+                        # Change parent directory permissions
+                        os.chmod(f'{path}', usr_flag)
 
                         # Think about other platforms
                         sys_clear()
@@ -156,33 +200,35 @@ Which \'Flag\' should be assigned to \'{basename}\':
                     sys.stdout.flush()
                     continue
 
+            # Exit script
+            sys.stdout.write('\nPermissions Change!\n\n')
+            sys.stdout.flush()
+            time.sleep(2)
+
         elif argv_pressent is True:
             self.loadingAnimation(loadingTxt, True)
 
             # Maby Itertools has a better and faster way
-            mode = []
-            for i in range(len(argv[1])):
-                for n in argv[1][i]:
-                    mode.append(flags[i][int(n)])
-            mode = sum(mode)
+            usr_flag = flag_interpreter(int(argv[1]), flags)
 
             # For each file in dir_path, change permission of file first.. // GOES ONLY 1 LEVEL DEEP FOR NOW
             for file in os.listdir(dir_path):
-                os.chmod(f'{dir_path}/{file}', mode)
+                os.chmod(f'{dir_path}/{file}', usr_flag)
 
             # ..then change permission of parent directory
-            os.chmod(f'{dir_path}', mode)
+            os.chmod(f'{dir_path}', usr_flag)
 
+            # Clear Screen
             sys_clear()
-            os.system(f"ls -d  -al */;cd {os.path.basename(dir_path)};ls -l *")
+            os.system(f"ls -d -al */")
 
             # Exit script
-            time.sleep(1)
             sys.stdout.write('\nPermissions Change!\n\n')
             sys.stdout.flush()
             time.sleep(2)
 
     def get_folder_path(self):
+        ''' Get folder path of which to change permissions. '''
 
         # Change directory to current file path.
         os.chdir(os.path.dirname(Path(__file__)))
@@ -195,25 +241,37 @@ Which \'Flag\' should be assigned to \'{basename}\':
         # If there's MORe then 1 directory next to this scirpt.
 
         if nr_of_dirs > 1:
+            while True:
 
-            sys.stdout.write(
-                f'\nFound {nr_of_dirs} directories!\n\n\nWhich one to change permissions on?\n\n')
-            dir_names = {}
+                sys.stdout.write(
+                    bcolors.BOLD + f'\nFound {nr_of_dirs} directories!' + bcolors.ENDC + bcolors.UNDERLINE + '\n\n\nWhich directory to change permissions on?\n\n' + bcolors.ENDC)
+                dir_names = {}
 
-            for n, i in enumerate(next(os.walk('.'))[1], 1):
-                sys.stdout.write(f'{n}. {i}\n')
-                sys.stdout.flush()
+                for n, i in enumerate(next(os.walk('.'))[1], 1):
+                    sys.stdout.write(
+                        bcolors.FAIL + f'{n}. {i}\n' + bcolors.ENDC)
+                    sys.stdout.flush()
 
-                # Append to dictionary to map numbers to directory names.
-                dir_names[n] = i
+                    # Append to dictionary to map numbers to directory names.
+                    dir_names[n] = i
 
-            answer = int(input('\n:> '))
+                try:
+                    answer = int(input('\n:> '))
+                    break
+                except ValueError:
+                    print('\nNot a valid input!')
+                    time.sleep(3)
 
+                    # Clear Screen
+                    sys_clear()
+
+                    continue
+            # Clear Screen
             sys_clear()
 
             for n in range(1, (nr_of_dirs + 1)):
                 if answer == n:
-                    return f'{os.getcwd()}/{dir_names[n]}'
+                    return f'{os.getcwd()}/{dir_names[n]}', f'{dir_names[n]}'
 
         # get DELETEME
         elif nr_of_dirs == 1:
@@ -224,21 +282,50 @@ Which \'Flag\' should be assigned to \'{basename}\':
             sys.stdout.flush()
 
 
+class bcolors:
+    ''' Text colors to use in the Terminal '''
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    # create a option to press ENTER and skip the rolling text #
+
+
 def flag_combinations():
     ''' Gets all CHMOD Flag combinations '''
-    mode = []
+    flag_combi = []
 
-    max_combinations = range(len(flags[0]))
-    result = itertools.combinations_with_replacement(max_combinations, 3)
+    nr_max_flag = range(len(flags[0]))
+    result = itertools.product(nr_max_flag, repeat=3)
 
     for i in result:
         j = str(i)
         k = j.replace("(", "")
         y = k. replace(")", "")
         s = y.replace(", ", "")
-        mode.append(s)
+        flag_combi.append(s)
 
-    return mode
+    return flag_combi
+
+
+def flag_interpreter(flag_int, flag_dict):
+    ''' Get correct flag '''
+
+    if len(argv) > 1:
+        flag_int = int(argv[1])
+
+    usr_flag = []
+
+    for i in range(len(str(flag_int))):
+        for n in str(flag_int)[i]:
+            usr_flag.append(flag_dict[i][int(n)])
+
+    return sum(usr_flag)
 
 
 def sys_clear():
@@ -246,21 +333,12 @@ def sys_clear():
     os.system('cls||clear')
 
 
-# Clears terminal screen
+# Clear Screen
 sys_clear()
 
 # Create object
 clss = MyClass()
 
-
 # If an argument variable is given..
-dir_path = clss.get_folder_path()
-clss.change_permissions(dir_path, script_argv)
-
-
-# Source: https://stackoverflow.com/questions/15607903/python-module-os-chmodfile-664-does-not-change-the-permission-to-rw-rw-r-bu#17776766
-
-# Note: Although Windows supports chmod(),
-# you can only set the file’s read-only flag with it
-# (via the stat.S_IWRITE and stat.S_IREAD constants or a corresponding integer value).
-# All other bits are ignored.
+dir_path, dir_name = clss.get_folder_path()
+clss.change_permissions(dir_path, dir_name, script_argv)
