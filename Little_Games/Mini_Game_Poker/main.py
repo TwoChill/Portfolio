af44b1551e898bba72d463ed300da22eb0d724fa
MdeFrance <mdefrance1988@gmail.com>
2020-02-22 00:18:09 +0100

    Adding Color With Fail
    I faild on Windows adding color to the cards

--- Little_Games/Mini_Game_Poker/main.py
+++ Little_Games/Mini_Game_Poker/main.py
@@ -3,6 +3,11 @@ import time
 sleeptime = 3
 test_loop = 4
 
+# # COMMENT IF TESTING #
+# clss.sys_clear()
+# nr_of_cards = 5
+# # COMMENT IF TESTING #
+
 try:
     # TESTING #
     while test_loop >= 0:
@@ -39,9 +44,15 @@ try:
 
         cards = clss.Cards()
         dealer = clss.Dealer()
-
+        combi_card_list = []
         # Create nr_of_cards
         ascii_cards, random_result = cards.create_cards(nr_of_cards)
+        card_list, suit_names, suit_symbols_dict = cards.get_all_combinations()
+        
+        # List of Suit Symbols
+        suit_symbols = [v for k, v in suit_symbols_dict.items()]
+
+        
 
         result = []
         for next_down in range(9):
@@ -49,18 +60,31 @@ try:
                 result.append('  '.join(ascii_cards[next_side][next_down]))
         print()
 
-        for card in result:
-            print(card)
+        loop = 0
+        start_index = 1
+        end_index = 2
 
-        # TESTING #
+        for line in result:
+            for i in range(1, 10, 3):
+                if line == result[i]:
+                    if line[1:6] == 'Joker':
+                        line = line.replace(line[1:6], clss.bcolors.ORANGE + line[1:6] + clss.bcolors.ENDC)
+                    if line[5:10] == 'Joker':
+                        line = line.replace(line[5:10], clss.bcolors.ORANGE + line[5:10] + clss.bcolors.ENDC)
+                    if line[5:6] in suit_symbols:
+                        line = line.replace(line[5:6], clss.bcolors.ORANGE + line[5:6] + clss.bcolors.ENDC)
+            print(line)
+
+
+        # TESTING and 1 TAB #
         input('\nPRESS ENTER')
         test_loop -= 1
 
         continue
 
     raise KeyboardInterrupt
+        # TESTING and 1 TAB #
 except KeyboardInterrupt:
     clss.sys_clear()
     print('\nGoodBye..\n')
     quit()
-        # TESTING #
